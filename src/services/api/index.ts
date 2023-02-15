adccb162a4dde48856982cd4b48961d131c5f8f2
Philip Lages <psilvaelages@gmail.com>
2023-02-15 10:15:39 -0300

    feat: adds license MIT to package.json and create .env.local file, to prepare to deploy

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Define a base axios const, avoiding the need to write the URL more than once
 const api = axios.create({
-  baseURL: 'https://www.zippia.com/api/jobs/'
+  baseURL: process.env.API_URL
 })
 
 export default api;
